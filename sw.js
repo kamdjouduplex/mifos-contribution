d074ec9c1e69ebd116f4710d7f5958bff1f90a73
kamdjou duplex <kamdjouduplex@outlook.com>
2018-06-29 22:50:46 +0100

    edited the sw file

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ var CURRENT_CACHES = 'currency-cache-'+ CACHE_VERSION;
 
 const STATIC_FILES =[
 
-	'/index.html',
+	'index.html',
 	'css/styles.min.css',
 	'js/main.min.js',
 	'assets/images/header.png'
@@ -43,9 +43,6 @@ self.addEventListener('fetch', event => {
         if (response) {
           	return response;
         }
-        /*if(event.request.url === api_url){
-        	console.log('ok currencies none caches');
-        }*/
         return fetch(event.request);
       })
       .catch( (error) => {
@@ -53,66 +50,3 @@ self.addEventListener('fetch', event => {
       })
   );
 });
-
-/*
-const cache_jon_data = (url) => {
-	fetch(url)
-	.then(res => res.json())
-	.then(json => {
-		currencies = json;
-		console.log(currencies);
-		setIndexDB(db_name, db_version, currencies);
-	});
-};
-
-
-
-const setIndexDB = (db_name, db_version, data) => {
-
-	if( 'indexedDB' in window){
-		const request = window.indexedDB.open(db_name, db_version);
-
-		request.onerror = function(event) {
-		  	// Do something with request.errorCode!
-		  	alert('Why don\'t you allow my app to use indexDB, we garanty you to not used any of your confidencial info');
-		};
-
-		request.onsuccess = function(event) {
-		  	// Do something with request.result!
-		  	db = event.target.result;
-		};
-
-
-		// This event is only implemented in recent browsers   
-		request.onupgradeneeded = function(event) { 
-		  // Save the appDatabase interface 
-		  let db = event.target.result;
-
-		  // Create an objectStore for this database
-		  let objectStore = db.createObjectStore("currencies", { keyPath: "id" });
-
-		  // Create an index to search currencies by id. We want to ensure that
-		  // no two currencies have the same id, so use a unique index.
-		  objectStore.createIndex("id", "id", { unique: true });
-
-		  // Use transaction oncomplete to make sure the objectStore creation is 
-		    // finished before adding data into it.
-		    objectStore.transaction.oncomplete = function(event) {
-		      // Store values in the newly created objectStore.
-		      let customerObjectStore = db.transaction("currencies", "readwrite").objectStore("currencies");
-		      const curs = data.results;
-		      for (const key in curs) {
-				const objs =curs[key];
-				console.log(objs);
-		      	customerObjectStore.add(objs);
-		      }
-		      data.forEach(function(customer) {
-		        
-		      });
-		    };
-		};
-
-	}else{
-		alert('Your browser doesn\'t support indexedDB');
-	}
-};*/
